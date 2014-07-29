2ba3646acac724e9933d91c11892eb78a9f13fec
Eric Hutson <ehutson@gmail.com>
2014-07-29 11:03:29 -0400

    testing using grunt-exec

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,35 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-
-    openssl: {
-      options: { 
-        cipher: 'cast5-cbc',
-        salt: '',
-        prefix: '',
-        affix: '.cast5-cbc'
+    exec: {
+      encrypt: {
+        cmd: function(password) {
+          return 'openssl cast5-cbc -e -in "./configs/database.json" -out "./configs/database.json.cast5" -k ' + password;
+        }
       },
-      myConfigs: ['./configs/*.json*']
+      decrypt: {
+        cmd: function(password) {
+          return 'openssl cast5-cbc -d -in "./configs/database.json.cast5" -out "./configs/database.json" -k ' + password;
+        }
+      },
+      echo_name: {
+        cmd: function(firstName, lastName) {
+          var formattedName = [
+            lastName.toUpperCase(),
+            firstName.toUpperCase()
+          ].join(', ');
+
+          return 'echo ' + formattedName;
+        }
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-openssl');
+  grunt.loadNpmTasks('grunt-exec');
 
-  grunt.registerTask('default', ['openssl']);
+  grunt.registerTask('encrypt', ['exec:encrypt']);
+  grunt.registerTask('decrypt', ['exec:decrypt']);
+
+
 };
 
